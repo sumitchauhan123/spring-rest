5c31dfe2eca277e91b0adec697b5bad03b7a370a
sumit <sumitchauhan1080@gmail.com>
2024-01-05 11:47:36 +0530

    first rest

---
+++ springrest/src/main/java/com/springrest/springrest/controller/SpringController.java
@@ -0,0 +1,31 @@
+package com.springrest.springrest.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class SpringController {
+    @GetMapping("/hello")
+    public String HelloWorld(){
+        return "hello world";
+    }
+    @GetMapping("hello/{name}")
+    public String Name(@PathVariable("name") String name){
+        return "Hello, "+ name;
+    }
+    //http://localhost:8080/hello/query?id=1&firstName=Sumit&lastName=Chauhan
+    @GetMapping("hello/query")
+        public String PathVar(@RequestParam("id") int id,
+                                 @RequestParam("firstName") String firstName,
+                                 @RequestParam("lastName") String lastName){
+        return id+" "+firstName+" "+lastName;
+    }
+    @PostMapping("hello/post/{firstName}/{lastName}")
+    public String PostMap(@PathVariable(value="lastName")String firstName1,
+                          @PathVariable(value="firstName")String lastName1){
+            return firstName1+" "+lastName1;
+    }
+    @PostMapping("json")
+    public String JsonReceiver(@RequestBody String jsondata){
+        return jsondata;
+    }
+}
