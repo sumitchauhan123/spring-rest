6af45625b0e0a11228c20b9b748eb26cdcdf417e
sumit <sumitchauhan1080@gmail.com>
2024-01-08 11:36:32 +0530

    used @RequestMapping

--- springrest/src/main/java/com/springrest/springrest/controller/SpringController.java
+++ springrest/src/main/java/com/springrest/springrest/controller/SpringController.java
@@ -5,23 +5,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("hello")
 public class SpringController {
-    @GetMapping("/hello")
+    @GetMapping()
     public String HelloWorld(){
         return "hello world";
     }
-    @GetMapping("hello/{name}")
+    @GetMapping("{name}")
     public String Name(@PathVariable("name") String name){
         return "Hello, "+ name;
     }
     //http://localhost:8080/hello/query?id=1&firstName=Sumit&lastName=Chauhan
-    @GetMapping("hello/query")
+    @GetMapping("query")
         public String PathVar(@RequestParam("id") int id,
                                  @RequestParam("firstName") String firstName,
                                  @RequestParam("lastName") String lastName){
         return id+" "+firstName+" "+lastName;
     }
-    @PostMapping("hello/post/{firstName}/{lastName}")
+    @PostMapping("post/{firstName}/{lastName}")
     public String PostMap(@PathVariable(value="lastName")String firstName1,
                           @PathVariable(value="firstName")String lastName1){
             return firstName1+" "+lastName1;
